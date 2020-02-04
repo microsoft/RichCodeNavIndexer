28dd440057ea62a8a98791a3d55da4add8f82a56
Andrew Arnott <andrew.arnott@microsoft.com>
2020-02-04 16:13:11 -0700

    Consume core NPM package and invoke

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
+import * as indexCore from '@cloudkernel/rich-code-nav-indexer-core';
+import { CachingTask, GitHubRepoInfo } from '@cloudkernel/rich-code-nav-indexer-core';
 import { factory } from 'cloudbuild-task-github-actions';
 
-console.log(`Building for ${factory.repo.uri}`);
+class GitHubCachingTask extends CachingTask {
+	protected getSourceControlInformationProvider(useRichNavMsbuildLog: boolean, languageInput?: string | undefined): indexCore.BaseRepoInfo {
+		return new GitHubRepoInfo(factory, useRichNavMsbuildLog, factory.inputs.getInput('repo-token'), languageInput);
+	}
+}
+
+new GitHubCachingTask(
+	factory,
+	{
+		serviceConnection: factory.inputs.getInput('service-token'),
+	}).run();
