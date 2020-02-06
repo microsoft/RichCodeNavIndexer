5834407b653489b6dd66b53abd4f724ef75c83b8
Jessica Petty <jepetty@microsoft.com>
2020-02-06 08:50:43 -0800

    Quick test of prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { factory } from 'cloudbuild-task-github-actions';
 
 class GitHubCachingTask extends CachingTask {
 	protected getSourceControlInformationProvider(useRichNavMsbuildLog: boolean, languageInput?: string | undefined): indexCore.BaseRepoInfo {
+		console.log('Testing prod endpoint...');
 		return new GitHubRepoInfo(factory, useRichNavMsbuildLog, factory.inputs.getInput('repo-token'), languageInput);
 	}
 }
