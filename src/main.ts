711842ea07b3f14eae2648cc64700b6d0d17e0ad
Briana <brianaeng@users.noreply.github.com>
2024-02-02 10:05:32 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github';
 class DeprecatedTask {
 	public async run() {
 		// Code Index deprecated, returning error on build.
-		github.warning(`As of January 2024, the RichCodeNav task is being deprecated. Please remove this task from your CI.`);
+		console.log(`As of January 2024, the RichCodeNav task is being deprecated. Please remove this task from your CI.`);
 	}
 }
 
