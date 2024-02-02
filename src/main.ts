477ec810dd87ab01528be9686e7926b67b9b560b
Briana Eng <breng@microsoft.com>
2024-02-02 10:47:33 -0800

    update name

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 class DeprecatedTask {
 	public async run() {
 		// Code Index deprecated, returning error on build.
-		console.log(`As of January 2024, the RichCodeNav task is being deprecated. Please remove this task from your CI.`);
+		console.log(`As of January 2024, the RichCodeNavIndexer task is being deprecated. Please remove this task from your CI.`);
 	}
 }
 
